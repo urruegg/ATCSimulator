fb558f1cbc3d866f7a46db56a1aba25c8b1f0c42
urruegg <urs.ruegg@microsoft.com>
2026-07-15 18:28:49 +0200

    feat(voice): in-process mock simulator adapter

---
+++ src/apis/AtcSim.VoiceAgentApi/Services/MockSimulatorAdapter.cs
@@ -0,0 +1,19 @@
+﻿using AtcSim.VoiceAgentApi.Contracts;
+
+namespace AtcSim.VoiceAgentApi.Services;
+
+public sealed class MockSimulatorAdapter
+{
+    private readonly object _gate = new();
+    public SimCommand? LastCommand { get; private set; }
+
+    public SimCommand? Dispatch(SimCommand command)
+    {
+        lock (_gate)
+        {
+            LastCommand = command;
+        }
+
+        return LastCommand;
+    }
+}
---
+++ tests/apis/AtcSim.VoiceAgentApi.Tests/FunctionCallHandlerTests.cs
@@ -0,0 +1,16 @@
+﻿using AtcSim.VoiceAgentApi.Contracts;
+using AtcSim.VoiceAgentApi.Services;
+using Xunit;
+
+namespace AtcSim.VoiceAgentApi.Tests;
+
+public class MockSimulatorAdapterTests
+{
+    [Fact]
+    public void Dispatch_records_last_valid_command()
+    {
+        var sim = new MockSimulatorAdapter();
+        sim.Dispatch(new SimCommand("SET_HEADING", new Dictionary<string, double> { ["heading"] = 90 }));
+        Assert.Equal("SET_HEADING", sim.LastCommand?.Type);
+    }
+}
